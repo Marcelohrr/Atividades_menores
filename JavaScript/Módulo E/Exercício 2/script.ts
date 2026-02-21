93d20d5fb74da8e73ecff7fadd99c5e36c56e0a3
Marcelo Henrique Rosa Reis <marcelohrr.ti@gmail.com>
2026-02-20 23:29:10 -0300

    19-20/02/2026 - Módulo E do curso JavaScript
    Exercícios do módulo E do curso JavaScript do Curso em Vídeo.

--- JavaScript/Módulo E/Exercício 2/script.ts
+++ JavaScript/Módulo E/Exercício 2/script.ts
@@ -1,26 +1,35 @@
-// TypeScript compilado em JavaScript
-const botao = document.querySelector('input#botaoGerar');
-const box = document.querySelector('div#resultado');
-if (!botao || !box)
-    throw new Error('Elementos não encontrados no DOM.');
-let boxEstilo = false;
-botao.addEventListener('click', function () { return gerarTabuada(box); });
-function gerarTabuada(container) {
-    const inputN = document.querySelector('input#txtn');
+/*
+const botao = document.querySelector<HTMLInputElement>('input#botaoGerar');
+const box = document.querySelector<HTMLDivElement>('div#resultado');
+
+if (!botao || !box) throw new Error('Elementos não encontrados no DOM.');
+
+let boxEstilo: boolean = false;
+
+botao.addEventListener('click', () => gerarTabuada(box));
+
+function gerarTabuada(container: HTMLDivElement): void {
+    const inputN = document.querySelector<HTMLInputElement>('input#txtn');
+
     if (!inputN || inputN.value === '') { // !inputN garante que o elemento existe no DOM e inputN.value === '' garante que o usuário digitou algo
         alert('Por favor, insira um valor!');
         return;
     }
+
     let n = Number(inputN.value);
+
     if (n <= 0) {
         alert('Por favor, insira um valor maior do que zero!');
         return;
     }
+
     if (n % 1 !== 0) { // !Number.isInteger(n) não funcionou
         alert('Por favor, insira um valor inteiro!');
         return;
     }
+
     container.innerHTML = '';
+
     if (!boxEstilo) {
         container.style.backgroundColor = 'lightcyan';
         container.style.marginTop = '20px';
@@ -29,16 +38,20 @@ function gerarTabuada(container) {
         container.style.textAlign = 'center';
         boxEstilo = true;
     }
+
     if (n > 100) {
         alert('Uau!');
     }
+
     for (let contador = 1; contador <= 10; contador++) {
         criarParagrafo(n, contador, container);
     }
 }
-function criarParagrafo(n, contador, container) {
+
+function criarParagrafo(n: number, contador: number, container: HTMLDivElement): void {
     const p = document.createElement('p');
-    p.textContent = "".concat(String(n), " x ").concat(contador, " = ").concat(n * contador);
+    p.textContent = `${String(n)} x ${contador} = ${n*contador}`;
     p.className = 'tabuada';
     container.appendChild(p);
 }
+*/
