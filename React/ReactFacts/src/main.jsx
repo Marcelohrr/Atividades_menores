a13cccb7452d22689582ca92a2396d0a1c5d2181
Marcelo Henrique Rosa Reis <marcelohrr.ti@gmail.com>
2026-07-01 00:32:04 -0300

    2026/06/25-2026/07/01 - React
    Anotações do vídeo "Learn React JS - Full Beginner’s Tutorial & Practice Projects" (freeCodeCamp.org, 2024).rg, 2024).

--- React/ReactFacts/src/main.jsx
+++ React/ReactFacts/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 
+/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+*/
+
+function Page() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react.svg" alt="React logo" />
+      </header>
+
+      <main>
+        <h1>Reasons I'm excited to learn React</h1>
+
+        <ol>
+          <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+          <li>I am more likely to get a job as a front end developer if I know React.</li>
+        </ol>
+      </main>
+
+      <footer>
+        <p>© 2026 Marcelo Henrique Rosa Reis development. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Page />
+);
