036f2cb5b8c4565d3546736161a22e859a0d4cb8
Marcelo Henrique Rosa Reis <marcelohrr.ti@gmail.com>
2026-07-06 14:03:47 -0300

    2026/06/25-2026/07/06 - React
    Anotações do vídeo "Learn React JS - Full Beginner’s Tutorial & Practice Projects" (freeCodeCamp.org, 2024).

--- React/ReactFacts/src/main.jsx
+++ React/ReactFacts/src/main.jsx
@@ -1,36 +1,62 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.jsx'
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.jsx';
 
 /*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
 */
+
+function Header() {
+  return (
+    <header className="header-maneiro">
+      <img src="src/assets/react.svg" alt="React logo" />
+
+      <nav>
+        <ul className="nav-list">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <main className="main-show-de-bola">
+      <h1 className="titulo-chique">Reasons I'm excited to learn React</h1>
+
+      <ol className="ol-brilhante">
+        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+        <li>I am more likely to get a job as a front end developer if I know React.</li>
+      </ol>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="footer-irado">
+      <p>© 2026 Marcelo Henrique Rosa Reis development. All rights reserved.</p>
+    </footer>
+  )
+}
 
 function Page() {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react.svg" alt="React logo" />
-      </header>
+    <>
+      <Header />
 
-      <main>
-        <h1>Reasons I'm excited to learn React</h1>
+      <MainContent />
 
-        <ol>
-          <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-          <li>I am more likely to get a job as a front end developer if I know React.</li>
-        </ol>
-      </main>
-
-      <footer>
-        <p>© 2026 Marcelo Henrique Rosa Reis development. All rights reserved.</p>
-      </footer>
-    </div>
+      <Footer />
+    </>
   );
 }
 
